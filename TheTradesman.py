da5cbe25f44c9de486599505782889bedbc47188
ANewman-Desktop <andrew.logan.newman@gmail.com>
2020-03-04 17:49:14 -0600

    Added audio on a lark, along with the 3DO version of 'At Doom's Gate' from DOOM. No longer crashes before name request

--- TheTradesman.py
+++ TheTradesman.py
@@ -1,7 +1,7 @@
 from os import system, name
 from time import sleep
 from TitleCards import titlescreen
-
+import winsound
 
 def clear():
     if name == 'nt':
@@ -11,11 +11,11 @@ def clear():
 # 80x24 is the common dimension for a terminal; spot 25 prints on the next line below? Read up on this
 # create better "print" function (self-scaling lines/frames with piping), store in separate python file
 
-
+winsound.PlaySound("ADG_3DO.wav", winsound.SND_ASYNC)
 clear()
 
 titlescreen()
-sleep()
+sleep(5)
 clear()
 
 print("==Welcome, Tradesman==")
