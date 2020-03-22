01484c4f81e521b9fed4a9cee028a13704643a6f
RedHatDrew <andrew.logan.newman@gmail.com>
2020-03-21 21:18:25 -0500

    Removed music (for now), implemented rudimentary buy functionality, cleaned up unused functions

--- TheTradesman.py
+++ TheTradesman.py
@@ -2,7 +2,7 @@ from os import system, name
 from time import sleep
 from TitleCards import companycard
 from TitleCards import gametitle
-import winsound
+# import winsound
 
 bool_exit = False
 
@@ -11,18 +11,12 @@ playername = ""
 player_asset_widget = 0
 player_asset_app = 0
 player_asset_stock = 0
-player_asset_dosh = 0
-player_day = 0
-player_location = ""
-
-def default_vars():
-    player_asset_widget = 0
-    player_asset_app = 0
-    player_asset_stock = 0
-    player_asset_dosh = 10000
-    player_day = 1
-    player_location = "coffee_shop"
-    return
+player_asset_dosh = 10000
+player_day = 1
+player_location = "coffee_shop"
+loc_price_widget = 10
+loc_price_app = 50
+loc_price_stock = 100
 
 
 def clear():
@@ -32,6 +26,7 @@ def clear():
         _ = system('clear')
 # 80x24 is the common dimension for a terminal; spot 25 prints on the next line below? Read up on this
 # create better "print" function (self-scaling lines/frames with piping), store in separate python file
+
 
 def startup():
     companycard()
@@ -44,20 +39,88 @@ def startup():
     return
 
 
-def save_data_check():
-    print("save data check coming soon!")
-    return
-
 def new_game():
     print("\n" * 10)
     print("==Welcome, Tradesman==")
     print("\n" * 5)
-    playername = input("What is your name?")  # Get input for user name
+    playername = input("What is your name?: ")  # Get input for user name
     clear()
     sleep(1)
     print("\n" * 10)
-    default_vars()
     print("Welcome, " + playername + ".")   # TODO: Move this to whenever you start playing, even when loading
+    return playername
+
+
+def travel_menu():
+    print("==========TRAVEL MENU==========")
+    print("Coming soon!")
+    sleep(1)    
+    return
+
+
+def buy_menu():
+    global player_asset_dosh
+    global player_asset_widget
+    global player_asset_app
+    global player_asset_stock
+    print("==========BUY MENU==========")
+    status_commerce()
+    print("1. Widget(s)")
+    print("2. App(s)")
+    print("3. Stock")
+    print("4. Back to Main Menu")
+    choice = input("What're ya buyin'?: ")
+    if choice == "1":
+        clear()
+        status_commerce()
+        str_amount_widget = input("Buying how many widgets?: ")
+        amount_widget = int(str_amount_widget)
+        dosh_diff = amount_widget * loc_price_widget
+        player_asset_dosh = player_asset_dosh - dosh_diff
+        player_asset_widget = player_asset_widget + amount_widget
+        print("Gaining " + str(amount_widget) + " widget(s), losing " + str(dosh_diff) + " dosh.")
+        sleep(1)
+        return
+        #TODO: Add purchase validation
+    if choice == "2":
+        clear()
+        status_commerce()
+        str_amount_app = input("Buying how many app(s)?: ")
+        amount_app = int(str_amount_app)
+        dosh_diff = amount_app * loc_price_app
+        player_asset_dosh = player_asset_dosh - dosh_diff
+        player_asset_app = player_asset_app + amount_app
+        print("Gaining " + str(amount_app) + " app(s), losing " + str(dosh_diff) + " dosh.")
+        sleep(1)
+        return
+    if choice == "3":
+        clear()
+        status_commerce()
+        str_amount_stock = input("Buying how much stock?: ")
+        amount_stock = int(str_amount_stock)
+        dosh_diff = amount_stock * loc_price_stock
+        player_asset_dosh = player_asset_dosh - dosh_diff
+        player_asset_stock = player_asset_stock + amount_stock
+        print("Gaining " + str(amount_stock) + " share(s) of stock, losing " + str(dosh_diff) + " dosh.")
+        sleep(1)
+        return
+    if choice == "4":
+        clear()
+        game_menu()
+    else:
+        print("Choice invalid. Try again.")
+        sleep(1)
+        clear()
+        buy_menu()
+        # TODO: add an else here to catch invalid answers
+        # Note: It seems to work anyway
+
+
+def sell_menu():
+    print("==========SELL MENU=========")
+    print("Coming Soon!")
+    sleep(1)
+    clear()
     return
 
 
@@ -69,9 +132,32 @@ def main_menu():
     return
 
 
+def status_commerce():
+    print("You are: " + playername + (" " * 10) + "|Dosh: " + str(player_asset_dosh) + (" " * 10) + "|Day: " + str(player_day))
+    print("        ------Inventory------|-------Prices------")
+    print("Widgets: " + str(player_asset_widget) + "         |       " + str(loc_price_widget))
+    print("Apps:    " + str(player_asset_app) + "         |       " + str(loc_price_app))
+    print("Stock:   " + str(player_asset_stock) + "         |       " + str(loc_price_stock))
+    print("\n" * 4)
+
+
 def game_menu_select():
+    print("1. Buy")
+    print("2. Sell")
+    print("3. Travel")
+    print("4. Exit")
     choice = input("Make a selection: ")
     if choice == "1":
+        clear()
+        buy_menu()
+        sleep(1)
+    if choice == "2":
+        clear()
+        sell_menu()
+    if choice == "3":
+        clear()
+        travel_menu()
+    if choice == "4":
         clear()
         print("Are you sure you want to exit The Tradesman?")
         print("1. Yes")
@@ -83,6 +169,7 @@ def game_menu_select():
             clear()
             game_menu()
         # TODO: add an else here to catch invalid answers
+        # Note: It seems to work anyway
     return
 
 
@@ -98,28 +185,28 @@ def load_game():
 
 
 def exit_game():
+    print("See you next time, " + playername + "!")
+    save_game()
+    sleep(2)
+    clear()
     exit()
 
 
 def game_menu():
-    print("==========CHOOSE AN OPTION==========")
-    print("1. Exit")
+    print("==========Main Menu==========")
+    status_commerce()
     game_menu_select()
     return
 
 
-winsound.PlaySound("ADG_3DO.wav", winsound.SND_ASYNC)   # Research OS agnostic sound solution
+# winsound.PlaySound("ADG_3DO.wav", winsound.SND_ASYNC)   # Research OS agnostic sound solution
 
 
 clear()
 
 startup()
-save_data_check()
-new_game()
+load_game()
+playername = new_game()
 
 while not bool_exit:
     game_menu()
-
-print("See you next time, " + playername + "!")
-sleep(2)
-clear()
